b787e1f085b3cb76d0ce87929c5ecc5173962b24
devLateef <lateefolatayo@gmail.com>
2026-08-17 05:54:03 +0100

    remove redundant code lines

--- backend/services/auth.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-from markupsafe import escape
-def home():
-    return render_template("landing.html")
-
-def hello(name):
-    return f"Nice to meet you {escape(name)}"
